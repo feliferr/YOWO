28aac15f6e1de47fe9468ea061aa7758a9a3b123
Felipe Ferreira <feliferrgo@gmail.com>
2021-05-02 00:51:51 -0300

    added scripts folder

---
+++ scripts/prepare-ava-dataset.sh
@@ -0,0 +1,108 @@
+
+# 1- Downloading AVA dataset
+
+DATA_DIR="data/ava/videos"
+
+if [[ ! -d "${DATA_DIR}" ]]; then
+  echo "${DATA_DIR} doesn't exist. Creating it.";
+  mkdir -p ${DATA_DIR}
+fi
+
+wget https://s3.amazonaws.com/ava-dataset/annotations/ava_file_names_trainval_v2.1.txt
+
+for line in $(cat ava_file_names_trainval_v2.1.txt)
+do
+  wget https://s3.amazonaws.com/ava-dataset/trainval/$line -P ${DATA_DIR}
+done
+
+
+# 2 - Cut each video from its 15th to 30th minute
+
+IN_DATA_DIR="data/ava/videos"
+OUT_DATA_DIR="data/ava/videos_15min"
+
+if [[ ! -d "${OUT_DATA_DIR}" ]]; then
+  echo "${OUT_DATA_DIR} doesn't exist. Creating it.";
+  mkdir -p ${OUT_DATA_DIR}
+fi
+
+for video in $(ls -A1 -U ${IN_DATA_DIR}/*)
+do
+  out_name="${OUT_DATA_DIR}/${video##*/}"
+  if [ ! -f "${out_name}" ]; then
+    ffmpeg -ss 900 -t 901 -i "${video}" "${out_name}"
+  fi
+done
+
+
+# 3 - Extract frames
+
+IN_DATA_DIR="data/ava/videos_15min"
+OUT_DATA_DIR="data/ava/frames"
+
+if [[ ! -d "${OUT_DATA_DIR}" ]]; then
+  echo "${OUT_DATA_DIR} doesn't exist. Creating it.";
+  mkdir -p ${OUT_DATA_DIR}
+fi
+
+for video in $(ls -A1 -U ${IN_DATA_DIR}/*)
+do
+  video_name=${video##*/}
+
+  if [[ $video_name = *".webm" ]]; then
+    video_name=${video_name::-5}
+  else
+    video_name=${video_name::-4}
+  fi
+
+  out_video_dir=${OUT_DATA_DIR}/${video_name}/
+  mkdir -p "${out_video_dir}"
+
+  out_name="${out_video_dir}/${video_name}_%06d.jpg"
+
+  ffmpeg -i "${video}" -r 30 -q:v 1 "${out_name}"
+done
+
+
+
+# 4 - Download annotations
+
+DATA_DIR="data/ava/annotations"
+
+if [[ ! -d "${DATA_DIR}" ]]; then
+  echo "${DATA_DIR} doesn't exist. Creating it.";
+  mkdir -p ${DATA_DIR}
+fi
+
+wget https://research.google.com/ava/download/ava_train_v2.1.csv -P ${DATA_DIR}
+wget https://research.google.com/ava/download/ava_val_v2.1.csv -P ${DATA_DIR}
+wget https://research.google.com/ava/download/ava_action_list_v2.1_for_activitynet_2018.pbtxt -P ${DATA_DIR}
+wget https://research.google.com/ava/download/ava_train_excluded_timestamps_v2.1.csv -P ${DATA_DIR}
+wget https://research.google.com/ava/download/ava_val_excluded_timestamps_v2.1.csv -P ${DATA_DIR}
+
+
+# 5 - Download "frame lists" (train, val) and put them in the "frame_lists" folder
+
+DATA_DIR="data/ava/frame_lists"
+
+if [[ ! -d "${DATA_DIR}" ]]; then
+  echo "${DATA_DIR} doesn't exist. Creating it.";
+  mkdir -p ${DATA_DIR}
+fi
+
+wget https://dl.fbaipublicfiles.com/video-long-term-feature-banks/data/ava/frame_lists/train.csv -P ${DATA_DIR}
+wget https://dl.fbaipublicfiles.com/video-long-term-feature-banks/data/ava/frame_lists/val.csv -P ${DATA_DIR}
+
+
+# 6 -  Download person boxes (train, val, test) and put them in the annotations folder
+
+DATA_DIR="data/ava/annotations"
+
+if [[ ! -d "${DATA_DIR}" ]]; then
+  echo "${DATA_DIR} doesn't exist. Creating it.";
+  mkdir -p ${DATA_DIR}
+fi
+
+wget https://dl.fbaipublicfiles.com/video-long-term-feature-banks/data/ava/annotations/ava_train_predicted_boxes.csv -P ${DATA_DIR}
+wget https://dl.fbaipublicfiles.com/video-long-term-feature-banks/data/ava/annotations/ava_val_predicted_boxes.csv -P ${DATA_DIR}
+wget https://dl.fbaipublicfiles.com/video-long-term-feature-banks/data/ava/annotations/ava_test_predicted_boxes.csv -P ${DATA_DIR}
